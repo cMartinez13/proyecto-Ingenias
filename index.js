8dbf9a9f69ab7fa3bc500cb36cdb07c1bb931fe8
cMartinez13 <camilamartinez8134@gmail.com>
2023-08-17 02:45:15 -0300

    Agregando ejs

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
-const { leerPeliculas } = require('./src/trailer.manager');
+const { leerPeliculas, obtenerReparto } = require('./src/trailer.manager');
 const app = express();
+const path = require("path");
 const PORT =process.env.PORT || 3008;
 let DB = [];
+
+//Exportamos el archivo trailer.manager.js
+const datos = require('./src/trailer.manager');
+
+
+
 //MIDDLEWARE
 dotenv.config();
 
@@ -15,14 +22,40 @@ app.use((req,res,next)=>{
     next();
 })
 
+// Inicializamos el motor de plantillas elegido
+
+app.set('view engine', 'ejs');
+app.use(express.static('views'));
 // SERVIDOR WEB 
 
 app.get('/', (req,res)=>{
-    res.send(DB)
+   const data = {
+    title: 'Bienvenido a Trailerflix',
+    message: 'Listado de peliculas y series disponibles',
+    products: leerPeliculas()
+   }   
+   res.render('trailerflix', data)
 })
 
+app.get('/reparto:act', (req, res)=>{
+    console.log("VALOR RECIBIDO:", req.params.reparto)
+    console.log(typeof req.params.reparto);
+    const actores = obtenerReparto();
+    res.send(actores);
+   
+})
+/*
+app.get('/:id',(req,res)=>{
+    console.log("VALOR RECIBIDO:", req.params.id)
+    console.log(typeof req.params.id)
+    const id = parseInt(req.params.id)
+    const fruta = obtenerFrutaFind(id)
+    res.send(fruta);
+})
+*/
 app.get('*', (req, res) => {
     res.status(404).send('Lo siento, la página que buscas no existe.'); 
 });
 
 app.listen(PORT, ()=>{console.log(`Node.js web server at port ${PORT} in running...`)} );
+
--- src/trailer.manager.js
+++ src/trailer.manager.js
@@ -6,5 +6,19 @@ function leerPeliculas(){
     return PELICULAS
 }
 
+function obtenerReparto(){
+    const datos = fs.readFileSync(__dirname + process.env.DATABASE_PATH, 'utf8' )
+    const actoresReparto = JSON.parse(datos)
+    const nombresActores = actoresReparto.map(actoresReparto => {
+        return {
+            titulo: actoresReparto.titulo,
+            reparto: actoresReparto.reparto
+        }
+    })
+    
+console.table(nombresActores)
 
-module.exports = {leerPeliculas}
+}
+
+module.exports = {leerPeliculas, obtenerReparto}
+
