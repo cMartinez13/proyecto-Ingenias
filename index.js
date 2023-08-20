bbd5e7f90cd157372f5bcb9cbdb5b0a674781a39
wiehl-valentina <valenwiehl@gmail.com>
2023-08-20 01:35:29 -0300

    add endpoints

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
-const { leerPeliculas} = require('./src/trailer.manager');
+const { leerPeliculas, filtrarTitulo, filtrarReparto } = require('./src/trailer.manager');
 const app = express();
 const path = require("path");
 const PORT =process.env.PORT || 3008;
@@ -40,15 +40,20 @@ app.get('/', (req,res)=>{
 })
 
 //RETORNA EL CATALOGO DE PELICULAS DE TRAILERFLIX
-
 app.get('/catalogo', (req,res)=>{
    const catalogo = leerPeliculas()
    res.json(catalogo);
    })
 
+// Retorna los elementos del catálogo que incluyen en su titulo el parámetro de la request
+app.get('/titulo/:title', (req,res) => {
+    const title = req.params.title.trim().toLowerCase();
+    const catalogo = filtrarTitulo(title);
+    (catalogo != []) ? res.json(catalogo).status(200)
+    : res.status(404).send('No se encuentran elementos que coincidan.'); 
+})
 
 // Retorna los datos de la categoria elegida (series/peliculas)
-
 app.get('/categoria/:cat', (req, res) => {
     const cat = req.params.cat.trim().toLowerCase();
     const nombre = leerPeliculas().filter(nombre => nombre.categoria.toLowerCase() == cat);
@@ -60,7 +65,6 @@ app.get('/categoria/:cat', (req, res) => {
 });
 
 // Retorna los datos del trailer de acuerdo al ID
-
 app.get('/trailer/:id', (req, res)=>{
     const id = req.params.id;
     const trailer = leerPeliculas().filter(trailer => trailer.id == id);
@@ -77,26 +81,16 @@ app.get('/trailer/:id', (req, res)=>{
     }
 })
 
-//Retorna los datos del reparto 
+//Retorna título y datos de reparto de los items de los cuales forme parte el actor/actriz que se ingresa por parámetro
 app.get('/reparto/:act', (req, res)=>{
     const act = req.params.act.trim().toLowerCase()
-    const reparto = leerPeliculas().filter(actor => actor.reparto.toLowerCase().trim().includes(act) == act);
-    if(reparto != ""){
-        const reparto1 = reparto.map(actor =>{
-            return{
-                titulo: actor.titulo,
-                reparto: actor.reparto
-            }
-        })
-        res.json(reparto1)
-    }else{
-        res.status(404).send('No se encuentra un actor/actriz con ese nombre')
-    }
+    const resultado = filtrarReparto(act);
+    (resultado != []) ? res.json(resultado).status(200)
+    : res.status(404).json('No se encontró el actor o actriz en el catálogo')
 })
 
 
-
-
+// Control de rutas inexistentes
 app.get('*', (req, res) => {
     res.status(404).send('Lo siento, la página que buscas no existe.'); 
 });
--- src/trailer.manager.js
+++ src/trailer.manager.js
@@ -1,12 +1,32 @@
 const fs = require('fs');
 
-
+// Lee la información almacenada en la DB
 function leerPeliculas(){
     const datos = fs.readFileSync(__dirname + process.env.DATABASE_PATH, 'utf8' )
     const PELICULAS = JSON.parse(datos)
     return PELICULAS
 }
 
+// Filtra el catálogo de acuerdo a los items que incluyen el parámetro en sus títulos
+function filtrarTitulo(title) {
+    const catalogo = leerPeliculas()
+    const filtrado = catalogo.filter(item => item.titulo.toLowerCase().includes(title));
+    return filtrado; 
+}
 
-module.exports = {leerPeliculas}
+// Filtra el catálogo de acuerdo a los items que incluyen en su reparto el valor enviado
+// por parámetro y después genera un nuevo array con los elementos coincidentes transformados
+function filtrarReparto(act) {
+    const catalogo = leerPeliculas();
+    const filtrado = catalogo.filter(item => item.reparto.toLowerCase().includes(act));
+    let resultado;
+    (filtrado != []) ? resultado = filtrado.map((item) => {
+        return {titulo: item.titulo, reparto: item.reparto};
+    })
+    : resultado = []; 
+    return resultado; 
+}
+
+
+module.exports = {leerPeliculas, filtrarReparto, filtrarTitulo}
 
